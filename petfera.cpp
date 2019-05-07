07ca6569dcda2a27821331410c0249661575ed2f
Felipe Mendonca Gurgel Bandeira <felipebandeira@A305-18L.imd.local>
2019-05-07 20:55:46 -0300

    add

--- petfera.cpp
+++ petfera.cpp
@@ -25,88 +25,42 @@ cout << "Todas as atividades relacionadas ao cadastro de Veterinários e Tratado
 */
 
 
+using namespace std;
 
-       	switch(num){
-            case 1: emp.EntradadadosEmpresa();
-            		emp.SaidadadosEmpresa();
-              break;
-            case 2: int qntfunc;
-					cout << "Quantos funcionarios serao cadastrados?" << endl;
-					cin >> qntfunc;
-            		func.Entradadadosfuncionais(qntfunc);
-            		func.Saidadadosfuncionais(qntfunc);
-              break;
-            case 3:
-            	break;
-            case 4:
-            	break;
-            case 5:
-            	break;
-            case 6:
-            	break;
-        }                
+int main( int argc, char const *argv[] )
+{
+    /* Abrindo os arquivos
+    fstream dados_funcionarios;
+    fstream dados_animais;
+    dados_funcionarios.exceptions ( fstream::failbit | fstream::badbit );
+    dados_animais.exceptions ( fstream::failbit | fstream::badbit );
+    try {
+        dados_funcionarios.open( "./data/cadastro_funcionarios.csv", fstream::in | fstream::out );
+        dados_animais.open( "./data/cadastro_animais.csv", fstream::in | fstream::out );
+    } 
+    catch (const ifstream::failure & e){
+        cerr << "Erro ao ler arquivo(s)\n";
+    }*/
+    
+    // Instanciando um petshop
+    Petshop pet_fera("Pet Fera");
 
+    // @TODO ler dados do cadastro
+    // pet_fera.carregar_dados();
 
-
-
-/*
-    cout << "Cadastre o nome da empresa" << endl;
-
-    string nomeemp;
-    getline(cin, nomeemp);
-    emp.setNomeEmp(nomeemp);
+    // Menu
+    //char option;
+    pet_fera.main_menu();
 
 
+    // Escrevendo saída para arquivo
+    std::ofstream arq_saida ("test.csv", std::ofstream::out);
+    
+    arq_saida << pet_fera;
+    arq_saida.close();
+    //dados_funcionarios.close();
+    //dados_animais.close();
+    
 
-    cout << "Quantos funcionarios serao adicionados a empresa?" << endl;
-    int quantfunc;
-    cin >> quantfunc;
-
-vector<Funcionario> func(quantfunc);
-
-    for(int i=0; i<func.size(); i++){
-
-	    func[i].Entradadadosfuncionais();
-
-	}
-
-	for(int i=0; i<func.size(); i++){
-
-	    func[i].Saidadadosfuncionais();
-
-	}
-
-
-
-*/
-    	
     return 0;
 }
-
-
-/*
-		cout << "Cadastre o nome do funcionario, salario e data de admissao" << endl;
-	    string nomefunc;
-	    getline(cin, nomefunc);
-	    func.setNomefunc(nomefunc); //nome do funcionario 
-
-	    cout << "Cadastre o salario do funcionario" << endl;
-	    float sal;
-	    cin >> sal;
-	    func.setSalario(sal); //salario do funcionario
-
-	    cout << "Cadastre a data de admissao do funcionario" << endl;
-	    int dia_adm, mes_adm, ano_adm;
-	    cin >> dia_adm;
-	    cin >> mes_adm;
-	    cin >> ano_adm;
-	    func.setDataAdm(dia_adm, mes_adm, ano_adm);//data de admissão
-
-	
-
-    cout << "Nome da empresa " << emp.getNomeEmp() << endl;
-    cout << "Nome do funcionario " << func.getNomefunc() << endl;
-    cout << "Salario do Funcionario  " << func.getSalario() << endl;
-    cout << "Data de admissao " << func.getDataAdm() << endl;
-
- */
